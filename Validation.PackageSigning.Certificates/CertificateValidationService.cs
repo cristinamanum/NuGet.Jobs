140376fa602e03f058925f6ddf420a38ce6e90bc
Loïc Sharma <sharma.loic@gmail.com>
2017-10-24 15:27:56 -0700

    Improved logic around trusted timestamp invalidation

--- Validation.PackageSigning.Certificates/CertificateValidationService.cs
+++ Validation.PackageSigning.Certificates/CertificateValidationService.cs
@@ -2,9 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -58,19 +58,19 @@ namespace Validation.PackageSigning.ValidateCertificate
             switch (result.Status)
             {
                 case CertificateStatus.Good:
-                    SaveGoodCertificateStatusAsync(validation);
+                    SaveGoodCertificateStatus(validation);
                     break;
 
                 case CertificateStatus.Invalid:
-                    SaveInvalidCertificateStatusAsync(validation);
+                    await SaveInvalidCertificateStatusAsync(validation);
                     break;
 
                 case CertificateStatus.Revoked:
-                    SaveRevokedCertificateStatusAsync(validation, result.RevocationTime.Value);
+                    await SaveRevokedCertificateStatusAsync(validation, result.RevocationTime.Value);
                     break;
 
                 case CertificateStatus.Unknown:
-                    SaveUnknownCertificateStatusAsync(validation);
+                    SaveUnknownCertificateStatus(validation);
                     break;
 
                 default:
@@ -98,7 +98,7 @@ namespace Validation.PackageSigning.ValidateCertificate
             }
         }
 
-        private void SaveGoodCertificateStatusAsync(CertificateValidation validation)
+        private void SaveGoodCertificateStatus(CertificateValidation validation)
         {
             validation.Certificate.Status = CertificateStatus.Good;
             validation.Certificate.StatusUpdateTime = null;
@@ -110,7 +110,7 @@ namespace Validation.PackageSigning.ValidateCertificate
             validation.Status = CertificateStatus.Good;
         }
 
-        private void SaveInvalidCertificateStatusAsync(CertificateValidation validation)
+        private async Task SaveInvalidCertificateStatusAsync(CertificateValidation validation)
         {
             validation.Certificate.Status = CertificateStatus.Invalid;
             validation.Certificate.StatusUpdateTime = null;
@@ -121,7 +121,9 @@ namespace Validation.PackageSigning.ValidateCertificate
 
             validation.Status = CertificateStatus.Invalid;
 
-            foreach (var signature in validation.Certificate.PackageSignatures)
+            var signatures = await FindSignatures(validation.Certificate);
+
+            foreach (var signature in signatures)
             {
                 if (signature.Status != PackageSignatureStatus.InGracePeriod)
                 {
@@ -133,7 +135,7 @@ namespace Validation.PackageSigning.ValidateCertificate
             }
         }
 
-        private void SaveRevokedCertificateStatusAsync(CertificateValidation validation, DateTime revocationTime)
+        private async Task SaveRevokedCertificateStatusAsync(CertificateValidation validation, DateTime revocationTime)
         {
             validation.Certificate.Status = CertificateStatus.Revoked;
             validation.Certificate.StatusUpdateTime = null;
@@ -144,10 +146,13 @@ namespace Validation.PackageSigning.ValidateCertificate
 
             validation.Status = CertificateStatus.Revoked;
 
-            foreach (var signature in validation.Certificate.PackageSignatures)
+            var signatures = await FindSignatures(validation.Certificate);
+
+            foreach (var signature in signatures)
             {
-                // TODO: If this is the timestamp authority certificate, ALWAYS revoke the package!!
-                if (signature.TrustedTimestamps.Any(t => t.Value < revocationTime)) continue;
+                // A revoked certificate does not necessarily invalidate a dependent signature. Skip signatures
+                // that should NOT be invalidated.
+                if (!RevokedCertificateInvalidatesSignature(validation.Certificate, signature)) continue;
 
                 if (signature.Status != PackageSignatureStatus.InGracePeriod)
                 {
@@ -159,7 +164,7 @@ namespace Validation.PackageSigning.ValidateCertificate
             }
         }
 
-        private void SaveUnknownCertificateStatusAsync(CertificateValidation validation)
+        private void SaveUnknownCertificateStatus(CertificateValidation validation)
         {
             validation.Certificate.ValidationFailures++;
 
@@ -175,8 +180,60 @@ namespace Validation.PackageSigning.ValidateCertificate
                 validation.Status = CertificateStatus.Invalid;
 
                 // TODO: ALERT
-                // TODO: This should be retried!
             }
+        }
+
+        /// <summary>
+        /// Find all package signatures that depend on the given certificate.
+        /// </summary>
+        /// <param name="certificate">The certificate whose signatures should be found.</param>
+        /// <returns>The signatures that depend on the given certificate.</returns>
+        private Task<List<PackageSignature>> FindSignatures(Certificate certificate)
+        {
+            return _context
+                        .PackageSignatures
+                        .Include(s => s.TrustedTimestamps)
+                        .Where(s => SignatureDependsOnCertificate(s, certificate))
+                        .ToListAsync();
+        }
+
+        /// <summary>
+        /// Determines whether a signature depends on the given certificate.
+        /// </summary>
+        /// <param name="signature">The signature that may depend on the certificate.</param>
+        /// <param name="certificate">The certificate that the signature may depend on.</param>
+        /// <returns>Whether the signature depend on the given certificate.</returns>
+        private bool SignatureDependsOnCertificate(PackageSignature signature, Certificate certificate)
+        {
+            if (signature.Certificate == certificate) return true;
+
+            return signature.TrustedTimestamps.Any(t => t.Certificate == certificate);
+        }
+
+        /// <summary>
+        /// Determines whether a revoked certificate should invalidate the signature.
+        /// </summary>
+        /// <param name="certificate">The revoked certificate.</param>
+        /// <param name="signature">The signature that may be invalidated.</param>
+        /// <returns>Whether the signature should be invalidated.</returns>
+        private bool RevokedCertificateInvalidatesSignature(Certificate certificate, PackageSignature signature)
+        {
+            // The signature may depend on a certificate in one of two ways: either the signature itself was signed with
+            // the certificate, or, the trusted timestamp authority used the certificate to sign its timestamp. Note that
+            // it is "possible" that both the signature and the trusted timestamp depend on the certificate.
+            if (signature.Certificate == certificate)
+            {
+                // The signature was signed using the certificate. Ensure that none of the trusted timestamps indicate
+                // that the signature was created before the certificate's invalidity date begins.
+                if (signature.TrustedTimestamps.Any(t => t.Value <= certificate.RevocationTime))
+                {
+                    return true;
+                }
+            }
+
+            // If any of the signature's trusted timestamps depend on the revoked certificate,
+            // the signature should be revoked.
+            return signature.TrustedTimestamps.Any(t => t.Certificate == certificate);
         }
     }
 }
